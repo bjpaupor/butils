d074dfbff93a32ac2ac5f60006fae0e346dd5630
Brandon Paupore <bjpaupor@gmail.com>
2024-07-04 03:32:37 -0500

    generate: ancestry and arranging support
    Signed-off-by: Brandon Paupore <bjpaupor@gmail.com>

--- generate.py
+++ generate.py
@@ -2,6 +2,194 @@ import sys
 import dice
 import ui
 
+def dwarven_qualified(scores):
+	result = True
+	if scores[0] < 8:
+		print("STRENGTH: {} is too low for a dwarf, a minimum of 8 is required".format(scores[0]),
+		      file=sys.stderr)
+		result = False
+	elif scores[4] < 11:
+		print("CONSTITUTION: {} is too low for a dwarf, a minimum of 12 (adjusted) is required".format(scores[4]),
+		      file=sys.stderr)
+		result = False
+	return result
+
+def elven_qualified(scores):
+	result = True
+	if scores[1] < 8:
+		print("INTELLIGENCE: {} is too low for an elf, a minimum of 8 is required".format(scores[1]),
+		      file=sys.stderr)
+		result = False
+	elif scores[3] < 6:
+		print("DEXTERITY: {} is too low for an elf, a minimum of 7 (adjusted) is required".format(scores[3]),
+		      file=sys.stderr)
+		result = False
+	elif scores[4] < 7:
+		print("CONSTITUTION: {} is too low for an elf, a minimum of 6 (adjusted) is required".format(scores[4]),
+		      file=sys.stderr)
+		result = False
+	elif scores[5] < 8:
+		print("CONSTITUTION: {} is too low for an elf, a minimum of 8 is required".format(scores[5]),
+		      file=sys.stderr)
+		result = False
+	return result
+
+def gnome_qualified(scores):
+	result = True
+	if scores[0] < 6:
+		print("STRENGTH: {} is too low for a gnome, a minimum of 6 is required".format(scores[0]),
+		      file=sys.stderr)
+		result = False
+	elif scores[1] < 7:
+		print("INTELLIGENCE: {} is too low for a gnome, a minimum of 7 is required".format(scores[1]),
+		      file=sys.stderr)
+		result = False
+	elif scores[4] < 8:
+		print("CONSTITUTION: {} is too low for a gnome, a minimum of 8 is required".format(scores[4]),
+		      file=sys.stderr)
+		result = False
+	return result
+
+def half_elven_qualified(scores):
+	result = True
+	if scores[1] < 4:
+		print("INTELLIGENCE: {} is too low for a half-elf, a minimum of 4 is required".format(scores[1]),
+		      file=sys.stderr)
+		result = False
+	elif scores[3] < 6:
+		print("DEXTERITY: {} is too low for a half-elf, a minimum of 6 is required".format(scores[3]),
+		      file=sys.stderr)
+		result = False
+	elif scores[4] < 6:
+		print("CONSTITUTION: {} is too low for a half-elf, a minimum of 6 is required".format(scores[4]),
+		      file=sys.stderr)
+		result = False
+	return result
+
+def halfling_qualified(scores):
+	result = True
+	if scores[0] < 7:
+		print("STRENGTH: {} is too low for a halfling, a minimum of 6 (adjusted) is required".format(scores[0]),
+		      file=sys.stderr)
+		result = False
+	elif scores[1] < 6:
+		print("INTELLIGENCE: {} is too low for a halfling, a minimum of 6 is required".format(scores[1]),
+		      file=sys.stderr)
+		result = False
+	elif scores[3] < 7:
+		print("DEXTERITY: {} is too low for a halfling, a minimum of 8 (adjusted) is required".format(scores[3]),
+		      file=sys.stderr)
+		result = False
+	elif scores[4] < 10:
+		print("CONSTITUTION: {} is too low for a halfling, a minimum of 10 is required".format(scores[4]),
+		      file=sys.stderr)
+		result = False
+	return result
+
+def half_orc_qualified(scores):
+	result = True
+	if scores[0] < 5:
+		print("STRENGTH: {} is too low for a half-orc, a minimum of 6 (adjusted) is required".format(scores[0]),
+		      file=sys.stderr)
+		result = False
+	elif scores[4] < 12:
+		print("CONSTITUTION: {} is too low for a half-orc, a minimum of 13 (adjusted) is required".format(scores[4]),
+		      file=sys.stderr)
+		result = False
+	return result
+
+def get_ancestry(scores):
+	while True:
+		ancestry = input("Which ancestry do they belong to?\n" \
+				 "Dwarven, Elven, Gnome, Half-Elven, Halfling, Half-Orc, or " \
+				 "Human\n")
+		if not ui.is_ancestry(ancestry):
+			print("Invalid ancestry option: {}\n".format(ancestry), file=sys.stderr)
+			continue
+		is_masc = ui.is_masc(input("What type of build do they have?\n" \
+					   "Masculine or Feminine\n"))
+		if ui.is_dwarven(ancestry) and dwarven_qualified(scores):
+			if scores[0] > 17 and not is_masc:
+				print("STRENGTH: {} is too high for a feminine dwarf, reducing to 17".format(scores[0]),
+				      file=sys.stderr)
+				scores[0] = 17
+			if scores[3] > 17:
+				print("DEXTERITY: {} is too high for a dwarf, reducing to 17".format(scores[3]),
+				      file=sys.stderr)
+				scores[3] = 17
+			scores[4] = scores[4] + 1
+			if int(scores[5]) > 16:
+				print("CHARISMA: {} is too high for a dwarf, reducing to 16 for non-dwarves".format(scores[5]),
+				      file=sys.stderr)
+				scores[5] = "16 ({})".format(scores[5])
+			else:
+				scores[5] = "{} ({})".format(scores[5] - 1, scores[5])
+		elif ui.is_elven(ancestry) and elven_qualified(scores):
+			if scores[0] > 16 and not is_masc:
+				print("STRENGTH: {} is too high for a feminine elf, reducing to 16".format(scores[0]),
+				      file=sys.stderr)
+				scores[0] = 16
+			scores[3] = scores[3] + 1
+			scores[4] = scores[4] - 1
+			break
+		elif ui.is_gnome(ancestry) and gnome_qualified(scores):
+			if scores[0] > 15 and not is_masc:
+				print("STRENGTH: {} is too high for a feminine gnome, reducing to 15".format(scores[0]),
+				      file=sys.stderr)
+				scores[0] = 15
+			break
+		elif ui.is_half_elven(ancestry) and half_elven_qualified(scores):
+			if scores[0] > 17 and not is_masc:
+				print("STRENGTH: {} is too high for a feminine half-elf, reducing to 17".format(scores[0]),
+				      file=sys.stderr)
+				scores[0] = 17
+			break
+		elif ui.is_halfling(ancestry) and halfling_qualified(scores):
+			if scores[0] > 15 and not is_masc:
+				print("STRENGTH: {} is too high for a feminine halfling, reducing to 14".format(scores[0]),
+				      file=sys.stderr)
+				scores[0] = 14
+			else:
+				scores[0] = scores[0] - 1
+			if scores[2] > 17:
+				print("WISDOM: {} is too high for a halfling, reducing to 17".format(scores[2]),
+				      file=sys.stderr)
+				scores[2] = 17
+			if scores[3] < 18:
+				scores[3] = scores[3] + 1
+			break
+		elif ui.is_half_orc(ancestry) and half_orc_qualified(scores):
+			if scores[0] < 18:
+				scores[0] = scores[0] + 1
+			if scores[1] > 17:
+				print("INTELLIGENCE: {} is too high for a half-orc, reducing to 17".format(scores[1]),
+				      file=sys.stderr)
+				scores[1] = 17
+			if scores[2] > 14:
+				print("WISDOM: {} is too high for a half-orc, reducing to 14".format(scores[2]),
+				      file=sys.stderr)
+				scores[2] = 14
+			if scores[3] > 17:
+				print("DEXTERITY: {} is too high for a half-orc, reducing to 17".format(scores[3]),
+				      file=sys.stderr)
+				scores[3] = 17
+			scores[4] = scores[4] + 1
+			if int(scores[5]) > 12:
+				print("CHARISMA: {} is too high for a half-orc, reducing to 12 for non-half-orcs".format(scores[5]),
+				      file=sys.stderr)
+				scores[5] = "12 ({})".format(scores[5])
+			else:
+				scores[5] = "{} ({})".format(scores[5] - 2, scores[5])
+			break
+		elif ui.is_human(ancestry):
+			break
+		else:
+			print("Invalid option for rolled ability scores: {}\n".format(ancestry), file=sys.stderr)
+
+	print("\nAdjusted scores:")
+	ui.print_scores(scores)
+	return ancestry, scores
+
 def get_germane_method():
 	while True:
 		# Only player options are reported
@@ -233,14 +421,84 @@ def method_10():
 		scores.append(roll1 + roll2 + roll3)
 	return scores
 
+def select_score(scores_rolled, scores, name):
+	while True:
+		print(scores_rolled)
+		score = int(input("{} = ?\n".format(name)))
+		if score in scores_rolled:
+			scores_rolled.remove(score)
+			scores.append(score)
+			break
+		else:
+			print("Score {} not in remaining rolled scores".format(score), file=sys.stderr)
+	return scores_rolled, scores
+
+def arrange_scores(raw_scores, method_scores):
+	scores = []
+	if not raw_scores[0]:
+		method_scores, scores = select_score(method_scores, scores, "STRENGTH")
+	else:
+		scores.append(raw_scores[0])
+	if not raw_scores[1]:
+		if len(method_scores) != 1:
+			method_scores, scores = select_score(method_scores, scores, "INTELLIGENCE")
+		else:
+			scores.append(method_scores[0])
+			method_scores.remove(method_scores[0])
+	else:
+		scores.append(raw_scores[1])
+	if not raw_scores[2]:
+		if len(method_scores) != 1:
+			method_scores, scores = select_score(method_scores, scores, "WISDOM")
+		else:
+			scores.append(method_scores[0])
+			method_scores.remove(method_scores[0])
+	else:
+		scores.append(raw_scores[2])
+	if not raw_scores[3]:
+		if len(method_scores) != 1:
+			method_scores, scores = select_score(method_scores, scores, "DEXTERITY")
+		else:
+			scores.append(method_scores[0])
+			method_scores.remove(method_scores[0])
+	else:
+		scores.append(raw_scores[3])
+	if not raw_scores[4]:
+		if len(method_scores) != 1:
+			method_scores, scores = select_score(method_scores, scores, "CONSTITUTION")
+		else:
+			scores.append(method_scores[0])
+			method_scores.remove(method_scores[0])
+	else:
+		scores.append(raw_scores[4])
+	if not raw_scores[5]:
+		scores.append(method_scores[0])
+	else:
+		scores.append(raw_scores[5])
+	return scores
+
 def generate_scores():
-	match get_method():
+	method = get_method()
+	scores = []
+	match method:
 		case 1:
 			print("Arrange these as the player desires:")
-			print(method_1())
+			scores_rolled = method_2()
+			scores_rolled, scores = select_score(scores_rolled, scores, "STRENGTH")
+			scores_rolled, scores = select_score(scores_rolled, scores, "INTELLIGENCE")
+			scores_rolled, scores = select_score(scores_rolled, scores, "WISDOM")
+			scores_rolled, scores = select_score(scores_rolled, scores, "DEXTERITY")
+			scores_rolled, scores = select_score(scores_rolled, scores, "CONSTITUTION")
+			scores.append(scores_rolled[0])
 		case 2:
 			print("Arrange these as the player desires:")
-			print(method_2())
+			scores_rolled = method_2()
+			scores_rolled, scores = select_score(scores_rolled, scores, "STRENGTH")
+			scores_rolled, scores = select_score(scores_rolled, scores, "INTELLIGENCE")
+			scores_rolled, scores = select_score(scores_rolled, scores, "WISDOM")
+			scores_rolled, scores = select_score(scores_rolled, scores, "DEXTERITY")
+			scores_rolled, scores = select_score(scores_rolled, scores, "CONSTITUTION")
+			scores.append(scores_rolled[0])
 		case 3:
 			scores = method_3()
 			print("STRENGTH: {}".format(scores[0]))
@@ -252,25 +510,49 @@ def generate_scores():
 		case 4:
 			score_sets = method_4()
 			print("Select the most desirable single set of scores:")
-			print("STRENGTH | INTELLIGENCE | WISDOM | DEXTERITY | CONSTITUTION | CHARISMA")
-			for scores in score_sets:
-				line = "{}         "
-				if scores[0] < 10:
-					line = line + " "
-				line = line + "{}             "
-				if scores[1] < 10:
-					line = line + " "
+			print("## | STRENGTH | INTELLIGENCE | WISDOM | DEXTERITY | CONSTITUTION | CHARISMA")
+			for i in range(1, len(score_sets) + 1):
+				scores = score_sets[i - 1]
+				line = "{} "
+				if i < 10:
+					line = line + " | "
+				else:
+					line = line + "| "
 				line = line + "{}       "
+				if scores[0] < 10:
+					line = line + " | "
+				else:
+					line = line + "| "
+				line = line + "{}           "
+				if scores[1] < 10:
+					line = line + " | "
+				else:
+					line = line + "| "
+				line = line + "{}     "
 				if scores[2] < 10:
-					line = line + " "
-				line = line + "{}          "
+					line = line + " | "
+				else:
+					line = line + "| "
+				line = line + "{}        "
 				if scores[3] < 10:
-					line = line + " "
-				line = line + "{}             "
+					line = line + " | "
+				else:
+					line = line + "| "
+				line = line + "{}           "
 				if scores[4] < 10:
-					line = line + " "
+					line = line + " | "
+				else:
+					line = line + "| "
 				line = line + "{}"
-				print(line.format(scores[0], scores[1], scores[2], scores[3], scores[4], scores[5]))
+				print(line.format(i, scores[0], scores[1], scores[2], scores[3], scores[4], scores[5]))
+			print("---------------------------------------------------------------------------\n")
+			while True:
+				selection = int(input("Which numbered set of scores should be used?\n"))
+				if selection > 0 and selection < 13:
+					scores = score_sets[selection - 1]
+					break
+				else:
+					print("Invalid selection, 1-12 expected", file=sys.stderr)
 		case 5:
 			scores = method_5()
 			print("STRENGTH: {}".format(scores[0]))
@@ -280,39 +562,39 @@ def generate_scores():
 			print("CONSTITUTION: {}".format(scores[4]))
 			print("CHARISMA: {}".format(scores[5]))
 		case 6:
-			scores, method1_scores = method_6()
-			if scores[0]:
-				print("STRENGTH: {}".format(scores[0]))
-			if scores[1]:
-				print("INTELLIGENCE: {}".format(scores[1]))
-			if scores[2]:
-				print("WISDOM: {}".format(scores[2]))
-			if scores[3]:
-				print("DEXTERITY: {}".format(scores[3]))
-			if scores[4]:
-				print("CONSTITUTION: {}".format(scores[4]))
-			if scores[5]:
-				print("CHARISMA: {}".format(scores[5]))
+			raw_scores, method1_scores = method_6()
+			if raw_scores[0]:
+				print("STRENGTH: {}".format(raw_scores[0]))
+			if raw_scores[1]:
+				print("INTELLIGENCE: {}".format(raw_scores[1]))
+			if raw_scores[2]:
+				print("WISDOM: {}".format(raw_scores[2]))
+			if raw_scores[3]:
+				print("DEXTERITY: {}".format(raw_scores[3]))
+			if raw_scores[4]:
+				print("CONSTITUTION: {}".format(raw_scores[4]))
+			if raw_scores[5]:
+				print("CHARISMA: {}".format(raw_scores[5]))
 			if method1_scores:
 				print("Arrange these as desired:")
-				print(method1_scores)
+			scores = arrange_scores(raw_scores, method1_scores)
 		case 7:
-			scores, method2_scores = method_7()
-			if scores[0]:
-				print("STRENGTH: {}".format(scores[0]))
-			if scores[1]:
-				print("INTELLIGENCE: {}".format(scores[1]))
-			if scores[2]:
-				print("WISDOM: {}".format(scores[2]))
-			if scores[3]:
-				print("DEXTERITY: {}".format(scores[3]))
-			if scores[4]:
-				print("CONSTITUTION: {}".format(scores[4]))
-			if scores[5]:
-				print("CHARISMA: {}".format(scores[5]))
+			raw_scores, method2_scores = method_7()
+			if raw_scores[0]:
+				print("STRENGTH: {}".format(raw_scores[0]))
+			if raw_scores[1]:
+				print("INTELLIGENCE: {}".format(raw_scores[1]))
+			if raw_scores[2]:
+				print("WISDOM: {}".format(raw_scores[2]))
+			if raw_scores[3]:
+				print("DEXTERITY: {}".format(raw_scores[3]))
+			if raw_scores[4]:
+				print("CONSTITUTION: {}".format(raw_scores[4]))
+			if raw_scores[5]:
+				print("CHARISMA: {}".format(raw_scores[5]))
 			if method2_scores:
 				print("Arrange these as desired:")
-				print(method2_scores)
+			scores = arrange_scores(raw_scores, method1_scores)
 		case 8:
 			scores = method_8()
 			print("STRENGTH: {}".format(scores[0]))
@@ -324,25 +606,49 @@ def generate_scores():
 		case 9:
 			score_sets = method_9()
 			print("Select the most desirable single set of scores:")
-			print("STRENGTH | INTELLIGENCE | WISDOM | DEXTERITY | CONSTITUTION | CHARISMA")
-			for scores in score_sets:
-				line = "{}         "
-				if scores[0] < 10:
-					line = line + " "
-				line = line + "{}             "
-				if scores[1] < 10:
-					line = line + " "
+			print("## | STRENGTH | INTELLIGENCE | WISDOM | DEXTERITY | CONSTITUTION | CHARISMA")
+			for i in range(1, len(score_sets) + 1):
+				scores = score_sets[i - 1]
+				line = "{} "
+				if i < 10:
+					line = line + " | "
+				else:
+					line = line + "| "
 				line = line + "{}       "
+				if scores[0] < 10:
+					line = line + " | "
+				else:
+					line = line + "| "
+				line = line + "{}           "
+				if scores[1] < 10:
+					line = line + " | "
+				else:
+					line = line + "| "
+				line = line + "{}     "
 				if scores[2] < 10:
-					line = line + " "
-				line = line + "{}          "
+					line = line + " | "
+				else:
+					line = line + "| "
+				line = line + "{}        "
 				if scores[3] < 10:
-					line = line + " "
-				line = line + "{}             "
+					line = line + " | "
+				else:
+					line = line + "| "
+				line = line + "{}           "
 				if scores[4] < 10:
-					line = line + " "
+					line = line + " | "
+				else:
+					line = line + "| "
 				line = line + "{}"
-				print(line.format(scores[0], scores[1], scores[2], scores[3], scores[4], scores[5]))
+				print(line.format(i, scores[0], scores[1], scores[2], scores[3], scores[4], scores[5]))
+			print("---------------------------------------------------------------------------\n")
+			while True:
+				selection = int(input("Which numbered set of scores should be used?\n"))
+				if selection > 0 and selection < 13:
+					scores = score_sets[selection - 1]
+					break
+				else:
+					print("Invalid selection, 1-12 expected", file=sys.stderr)
 		case 10:
 			scores = method_10()
 			print("STRENGTH: {}".format(scores[0]))
@@ -352,10 +658,14 @@ def generate_scores():
 			print("CONSTITUTION: {}".format(scores[4]))
 			print("CHARISMA: {}".format(scores[5]))
 
-	return scores
+	print("\nInitial scores:")
+	ui.print_scores(scores)
+
+	return scores, method < 5
 
 def main():
-	scores = generate_scores()
+	scores, is_pc = generate_scores()
+	ancestry, scores = get_ancestry(scores)
 	return 0
 
 if __name__ == '__main__':
--- ui.py
+++ ui.py
@@ -1,3 +1,67 @@
 
 def is_negative(option):
 	return "no" in option or "n" in option or "No" in option or "N" in option
+
+def is_ancestry(option):
+	return is_dwarven(option) or is_elven(option) or is_gnome(option) or \
+	       is_half_elven(option) or is_halfling(option) or is_half_orc(option) or \
+	       is_human(option)
+
+def is_dwarven(option):
+	return "Dwarven" in option or "dwarven" in option or "Dwarf" in option or "dwarf" in option
+
+# equal to avoid miscompare with Half-Elven characters
+def is_elven(option):
+	return "Elven" == option or "elven" == option or "Elf" == option or "elf" == option
+
+def is_gnome(option):
+	return "Gnome" in option or "gnome" in option or "Gnomish" in option or "gnomish" in option
+
+def is_half_elven(option):
+	return "Half-Elven" in option or "Half-elven" in option or "half-elven" in option or \
+	       "Half-Elf" in option or "Half-elf" in option or "half-elf" in option
+
+def is_halfling(option):
+	return "Halfling" in option or "halfling" in option
+
+def is_half_orc(option):
+	return "Half-Orc" in option or "Half-orc" in option or "half-orc" in option or \
+	       "Half-Orcish" in option or "Half-orcish" in option or "half-orcish" in option
+
+def is_human(option):
+	return "Human" in option or "human" in option
+
+def is_masc(option):
+	return "Masculine" in option or "masculine" in option or "Masc" in option or \
+	       "masc" in option or "M" == option or "m" == option
+
+def print_scores(scores):
+	print("STRENGTH | INTELLIGENCE | WISDOM | DEXTERITY | CONSTITUTION | CHARISMA")
+	line = "{}       "
+	if scores[0] < 10:
+		line = line + " | "
+	else:
+		line = line + "| "
+	line = line + "{}           "
+	if scores[1] < 10:
+		line = line + " | "
+	else:
+		line = line + "| "
+	line = line + "{}     "
+	if scores[2] < 10:
+		line = line + " | "
+	else:
+		line = line + "| "
+	line = line + "{}        "
+	if scores[3] < 10:
+		line = line + " | "
+	else:
+		line = line + "| "
+	line = line + "{}           "
+	if scores[4] < 10:
+		line = line + " | "
+	else:
+		line = line + "| "
+	line = line + "{}"
+	print(line.format(scores[0], scores[1], scores[2], scores[3], scores[4], scores[5]))
+	print("----------------------------------------------------------------------\n")
